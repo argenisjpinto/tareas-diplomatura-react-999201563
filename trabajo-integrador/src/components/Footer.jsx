74f039341ba08a9dd5620b7f41e9b7c7d5500d70
Argenis Pinto <140538717+argenisjpinto@users.noreply.github.com>
2026-02-08 14:10:58 -0300

    Delete trabajo-integrador directory

--- trabajo-integrador/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer>
-            <p>&copy; Trabajo Integrador React 2026 · Desarrollado por Argenis Pinto. Todos los derechos reservados.</p>
-        </footer>
-    )
-}
-
-export { Footer }
